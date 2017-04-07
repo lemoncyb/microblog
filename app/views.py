1eb250f6e3ae2e6c07ec791a5380f7bd65708892
Yingbo Cui <cuiyingbomail@163.com>
2017-04-07 10:22:12 -0400

    Part II 1

--- app/views.py
+++ app/views.py
@@ -4,4 +4,13 @@ from app import app
 @app.route('/index')
 def index():
     user = {'nickname': 'Miguel'}  # fake user
-    return "Hello, World!"
+    return '''
+<html>
+  <head>
+    <title>Home Page</title>
+  </head>
+  <body>
+    <h1>Hello, ''' + user['nickname'] + '''</h1>
+  </body>
+</html>
+'''
