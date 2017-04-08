688f3201613fc859e8d2565be7c1ca753f0711b2
Yingbo Cui <cuiyingbomail@163.com>
2017-04-07 23:17:09 -0400

    Part V: The index view

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,17 @@
 from flask import Flask
+from config import basedir
 from flask_sqlalchemy import SQLAlchemy
+import os
+from flask_login import LoginManager
+from flask_openid import OpenID
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 
 from app import views, models
