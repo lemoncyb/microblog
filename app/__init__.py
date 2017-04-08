81c334edf1eb0835579297bd32e7575dfdede386
Yingbo Cui <cuiyingbomail@163.com>
2017-04-08 12:09:58 -0400

    naive bootstap try

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,11 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 from flask_login import LoginManager
 from flask_openid import OpenID
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object('config')
+Bootstrap(app)
 db = SQLAlchemy(app)
 
 lm = LoginManager()
